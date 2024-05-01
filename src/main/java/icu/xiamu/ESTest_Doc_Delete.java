2108f64e1e4c76655449ed52b8e27b24d65d15b1
肉豆蔻 <1023876294@qq.com>
2024-05-01 14:31:13 +0800

    8.文档-删除

---
+++ src/main/java/icu/xiamu/ESTest_Doc_Delete.java
@@ -0,0 +1,27 @@
+package icu.xiamu;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+
+import java.io.IOException;
+
+public class ESTest_Doc_Delete {
+    public static void main(String[] args) throws IOException {
+        RestHighLevelClient esClient = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("192.168.1.100", 9200, "http"))
+        );
+
+        // 删除数据
+        DeleteRequest request = new DeleteRequest();
+        request.index("user").id("1001");
+
+        DeleteResponse response = esClient.delete(request, RequestOptions.DEFAULT);
+        System.out.println(response.toString());
+
+        esClient.close();
+    }
+}
